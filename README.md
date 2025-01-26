8a4fe5c566344614bef8d695d958630937ef4b2d
Gus Thomas <augustusrthomas@gmail.com>
2025-01-26 13:01:40 -0600

    cleaning up

--- README.md
+++ README.md
@@ -2,12 +2,37 @@
 This is a small experiment comparing Rust to Python.
 
 ## Specification
+Goal: pathfind in a weighted graph using the Bellman-Ford algorithm.
+
 Input: 
-    - nodes.csv: nodes and their x, y, z coordinates
-    - edges.csv: directed edges
+    - nodes.csv: nodes and their x, y, z coordinates in Euclidean space.
+    - edges.csv: directed edges.
+    - source & target: stdin
+
 Output: 
     - shortest path between nodes; stdout.
+
+Example Input:
+Find me a path from cat to meow
+```
+/path/to/some/executable cat meow
+```
+
 Example Output:
+The shortest path from cat to meow is ~29 units via b and a.
 ```
 -> cat -> b -> a -> meow 29.83186638883156
 ```
+
+## Reflection & Comparison
+### Similarities
+Both Rust and Python rely on third party libraries for reading Command Seperated Value (csv) files. Both use the standard library for writing the standard output.
+In both cases the logic of the program is the same: parse the input into a useful data structure representation, process the data structure, output the result. In both cases a dictionary/hashmap was used to represent the directed acyclic graph from source to target. Both implementations relied on data structures: in Python the data structures were built-in, whereas in Rust I had to specify the structures myself.
+### Differences
+By far the most stark difference is error handling: Rust won't even compile without every edge case being handled at compile time, which makes its code air-tight. I did find that development time was 3 to 4 times longer because of that, although this might be confounded by my understanding/comfort with the language. I also used more lines of code in Rust since it is less expressive and more cumbersome with error catching requirements.
+
+Despite that I think Rust still "wins." Python is a prototyping language which is expressive enough to quickly sketch the behavior of a program. But Rust's error messages and rust-analyzer are so helpful that I think it would be better to write production-quality code in Rust. This is because errors can be caught at compile-time as opposed to hitting exceptions during deployment. 
+
+I liked that Rust managed dependencies in a ``transparent" way. I don't know how many times I have worked on a Python bug only to find that I was using a different dependency version than someone else. The Crates.toml file is very useful in that regard.
+
+I found that the Rust linter could catch unused or unneeded data structures, which would be very good for bug catching in a larger project. The compiler also prefers pass-by-reference over pass-by-value, but enforces data ownership requirements; I do not know enough about this yet, but it encourages me to write cleaner code I think.
--- route/src/main.rs
+++ route/src/main.rs
@@ -1,12 +1,9 @@
 use std::{
-    //env,
+    env,
     collections::HashMap,
     error::Error,
-    //ffi::OsString,
     fs::File,
-    //process,
 };
-
 struct Point {
     x: f32,
     y: f32,
@@ -24,20 +21,20 @@ struct Edge {
 }
 
 struct Graph {
-    nodes: Vec<Node>,
+    nodes: HashMap<String, Node>,
     edges: Vec<Edge>,
 }
 
 // helper distance method
-fn dist(source: Node, destination: Node) -> f32 {
+fn r(source: &Node, destination: &Node) -> f32 {
     // cleaner way to do this? lol
     let del_x: f32 = destination.coord.x - source.coord.x;
     let del_y: f32 = destination.coord.y - source.coord.y;
     let del_z: f32 = destination.coord.z - source.coord.z;
     (del_x.powf(2.0) + del_y.powf(2.0) + del_z.powf(2.0)).sqrt()
 }
-fn read_nodes(file_path: &str) -> Result<Vec<Node>, Box<dyn Error>> {
-    let mut n: Vec<Node> = vec![];
+fn read_nodes(file_path: &str) -> Result<HashMap<String, Node>, Box<dyn Error>> {
+    let mut n: HashMap<String, Node> = HashMap::new();
     let file = File::open(file_path)?;
     let mut rdr = csv::Reader::from_reader(file);
     for result in rdr.records() {
@@ -45,7 +42,7 @@ fn read_nodes(file_path: &str) -> Result<Vec<Node>, Box<dyn Error>> {
             Ok(r) => Node{label: r[0].to_string(), coord: Point{x: r[1].parse::<f32>().unwrap(), y: r[2].parse::<f32>().unwrap(), z: r[3].parse::<f32>().unwrap()}},
             Err(_e) => panic!("why"),
         };
-        n.push(node);
+        n.insert(node.label.clone(), node);
     }
     return Ok(n);
 }
@@ -66,20 +63,45 @@ fn read_edges(file_path: &str) -> Result<Vec<Edge>, Box<dyn Error>> {
 
 fn main() {
     // IO
-    let g: Graph = Graph{nodes: read_nodes("../nodes.csv").expect("what"), edges: read_edges("../edges.csv").expect("what")};
+    let args: Vec<String> = env::args().collect();
+    let g: Graph = Graph{nodes: read_nodes("../nodes.csv").expect("nodes file not found (place in ../)"), edges: read_edges("../edges.csv").expect("edges file not found (place in ../)")};
+    let mut source: String = args[1].clone();
+    let target: String = args[2].clone();
     // Bellman-Ford
-    let source: String = String::from("cat");
-    let target: String = String::from("meow");
     let mut dist: HashMap<String, f32> = HashMap::new();
+    let mut dag: HashMap<String, String> = HashMap::new();
     // init
-    for x in &g.nodes {
-        dist.insert(String::clone(&x.label), 10000000000000.0);
+    for key in g.nodes.keys() {
+        dist.insert(key.clone(), 10000000000000.0);
     }
+    dist.insert(source.clone(), 0.0);
 
-
-    for x in &g.nodes {
+    for _x in &g.nodes {
         for y in &g.edges {
-
+            let dist_u: f32 = match dist.get(&y.u) {
+                Some(distance) => distance.clone(),
+                None => panic!("{} has no distance", &y.u)
+            };
+            let dist_v: f32 = match dist.get(&y.v) {
+                Some(distance) => distance.clone(),
+                None => panic!("{} has no distance", &y.v)
+            };
+            let w: f32 = r(g.nodes.get(&y.u).expect("node not found"), g.nodes.get(&y.v).expect("node not found"));
+            if dist_u + w < dist_v {
+                dist.insert(y.v.clone(), dist_u + w);
+                dag.insert(y.u.clone(), y.v.clone());
+            }
         }
     }
+    // IO
+    print!("-> {} ", source);
+    while source !=  target {
+        let next = match dag.get(&source) {
+            Some(next) => next,
+            None => panic!("no!")
+        };
+        print!("-> {} ", next);
+        source = next.to_string();
+    }
+    print!("{}", dist.get(&target).expect("target unreachable"));
 }
